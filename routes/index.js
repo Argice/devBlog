c0ec8d8f8704f099e7e23ad05c569395dd328ba1
Argice <736543458@qq.com>
2021-01-22 16:51:07 +0800

    界面构建完成：首页、详情页、登录页面

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('abc_default', { title: 'Express' });
 });
+// 登录路由配置
+router.get('/login', function (req, res) {
+  res.render('login', {})
+})
+
+// 文章详情页路由配置
+router.get('/article', function (req, res) {
+  res.render('abc_article', {})
+})
 
 module.exports = router;
