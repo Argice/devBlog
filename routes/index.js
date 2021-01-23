b9a611c033e5ce2a7bec65b2bc31a5b3ca160ecf
Argice <736543458@qq.com>
2021-01-23 15:12:53 +0800

    用户信息接口配置成功

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('abc_default', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
 // 登录路由配置
 router.get('/login', function (req, res) {
   res.render('login', {})
+})
+// 注册路由配置
+router.get('/register', function (req, res) {
+  res.render('register', {})
 })
 
 // 文章详情页路由配置
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
+
+// 实现用户提交信息，注册事项
+// Response   响应
+// Request    请求
+router.post('/addUser', (req, res, next) => {
+
+  console.log(req.body);
+  // 用户填写的表单信息可以通过req.body获取到
+
+  // res.send('点击注册了')
+  // console.log('点击注册了')
+
+})
 
 module.exports = router;
